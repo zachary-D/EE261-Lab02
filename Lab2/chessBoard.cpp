4d2bd851a120d71018437d137f7fb3cb302b4081
Unknown <zacharyd1@live.com>
2017-09-08 13:02:31 -0400

    Complete through exercise 3

--- Lab2/chessBoard.cpp
+++ Lab2/chessBoard.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 
 
-const string BLACK = "********";  // Define a line of a black square
+const string BLACK = "$$$$$$$$";  // Define a line of a black square
+const string EDGE = "$      $";		//Define a middle of a filled square
 const string WHITE = "        ";  // Define a line of a white square
 
 int main()
@@ -20,65 +21,94 @@ int main()
 								// Create a white-black row by concatenating the basic strings
 	whiteRow = WHITE + BLACK + WHITE + BLACK +
 		WHITE + BLACK + WHITE + BLACK;
+	string whiteRow_mid = WHITE + EDGE + WHITE + EDGE +	//A row begging with a white square that composes the middle of a square
+		WHITE + EDGE + WHITE + EDGE;
+
 
 	// Create a black-white row by concatenating the basic strings
 	blackRow = BLACK + WHITE + BLACK + WHITE +
 		BLACK + WHITE + BLACK + WHITE;
+	string blackRow_mid = EDGE + WHITE + EDGE + WHITE +
+		EDGE + WHITE + EDGE + WHITE;
 
 	// Print five white-black rows 
 	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl; 
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
 	cout << whiteRow << endl;
 
 	// Print five black-white rows
 	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-
-	// Print five white-black rows 
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-
-	// Print five black-white rows
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
 	cout << blackRow << endl;
 
 	// Print five white-black rows 
 	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
 	cout << whiteRow << endl;
 
 	// Print five black-white rows
 	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
-	cout << blackRow << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
 	cout << blackRow << endl;
 
 	// Print five white-black rows 
 	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
-	cout << whiteRow << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
 	cout << whiteRow << endl;
 
 	// Print five black-white rows
 	cout << blackRow << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
 	cout << blackRow << endl;
+
+	// Print five white-black rows 
+	cout << whiteRow << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow_mid << endl;
+	cout << whiteRow << endl;
+
+	// Print five black-white rows
 	cout << blackRow << endl;
-	cout << blackRow << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
+	cout << blackRow_mid << endl;
 	cout << blackRow << endl;
 	cin.ignore();
 	return 0;
