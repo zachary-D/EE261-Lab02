9b64160b0b726d843c416ea023498b9d558c1daa
Unknown <zacharyd1@live.com>
2017-09-08 12:24:17 -0400

    Added solution and compiled given code

---
+++ Lab2/error.cpp
@@ -0,0 +1,14 @@
+// Program Error prints 4 lines on the screen.
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout <<"This program demonstrates " <<endl;
+  cout <<"how a missing semicolon can "
+
+  cout <<"cause a problem." <<endl;
+  cout <<"*********************" <<endl;
+  return 0;
+}
---
+++ Lab2/firstone.cpp
@@ -0,0 +1,13 @@
+// This program prints my name on the screen.
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout <<"**********************" <<endl;
+  cout <<"My name is ";
+  cout <<"Nell Dale" <<endl;
+  cout <<"*********************" <<endl;
+  return 0;
+}
